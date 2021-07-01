a75633e794bd8cee660e6d587cf13f9180505f38
Bruk <dp170693paa@gmail.com>
2021-07-01 13:45:19 +0300

    Update HW - 2.rb

--- HW - 2.rb
+++ HW - 2.rb
@@ -14,3 +14,24 @@ end
 short_word("lets talk about javascript the best language")
 
 #6
+
+
+
+#HW - 3 - ?
+#Task 1: Sum of Minimums!
+a = [6,42,345,79,1]
+puts a.compact.inject(:+)
+
+#Task 2: Two Oldest Ages
+
+def two_oldest_ages(ages)
+  ages.sort[-1..100]
+  if ages >100
+    puts "Возраст не должен привышать 100 лет"
+  end
+
+end
+puts "Введите возраст:"
+ages = gets
+result = two_oldest_ages
+puts ("Даты: " + result)
