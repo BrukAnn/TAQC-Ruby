44ee0395b447ecc37515e3cfe18395ce1a7f4b26
Bruk <dp170693paa@gmail.com>
2021-07-16 13:47:00 +0300

    Create task1.rb

---
+++ main/src/task1.rb
@@ -0,0 +1,21 @@
+def chessboard(l, w, symbol)
+  arr = []
+  for i in 0..l
+    for j in 0..w
+      if (i+j) % 2 == 0
+        arr << (symbol)
+      else
+        arr << (' ')
+      end
+    end
+  arr << ("\n")
+end
+return arr
+end
+
+def start (l, w, symbol)
+  str = chessboard(l, w, symbol)
+  return str.join
+end
+
+print start(5, 15, "*")
