216c53ddfd976d1231a4780aadccc7e9b6fd0bba
Bruk <dp170693paa@gmail.com>
2021-07-19 14:17:31 +0300

    Create main.rb

---
+++ DEMO-1/main.rb
@@ -0,0 +1,27 @@
+require_relative "src/task1.rb"
+require_relative "src/task2.rb"
+require_relative "src/task5.rb"
+require_relative "src/task6.rb"
+require_relative "src/task7.rb"
+
+
+#1 Шахматная доска
+#print start(5, 15, "*")
+
+#2 Анализ конвертов
+envp_1 = { "a" => 123.0 "b" => 10.0}
+envp_2 = { "c" => 10.0, "d" => 6.0}
+#puts attach(envp_1, envp_2)
+
+
+#5 Lucky tickets
+#print tickets(0, 0)
+
+
+#6 Последовательность чисел
+#print square(7, 5)
+
+
+#7 Фибоначчи
+n = {:min => 9, :max => 20}
+#print fib(n)
